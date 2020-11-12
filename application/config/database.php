2320ee6c303aac5552e3a5d50eecb72fa5e6c7b6
Yogesh Basnet <uguess001@gmail.com>
2020-11-12 23:11:45 +0545

    changes on view

--- application/config/database.php
+++ application/config/database.php
@@ -50,4 +50,55 @@
 // 	'failover' => array(),
 // 	'save_queries' => true
 // );*/
-// 
+// <?php
+		defined('BASEPATH') OR exit('No direct script access allowed');
+		$active_group = 'default';
+		$query_builder = true;
+		$dbhost = 'localhost';
+		$db['default'] = array(
+			'dsn'	=> 'mysql:host=' . $dbhost . ';dbname=rbcll_website',
+			'hostname' => $dbhost,
+			'username' => 'rbcll_db',
+			'password' => '+-y^pS}qg@og',
+			'database' => 'rbcll_website', 
+			'dbdriver' => 'mysqli',
+			'dbprefix' => '',
+			'pconnect' => true,
+			'db_debug' => true,
+			'cache_on' => false,
+			'cachedir' => '',
+			'char_set' => 'utf8',
+			'dbcollat' => 'utf8_general_ci',
+			'swap_pre' => '',
+			'encrypt' => false,
+			'compress' => false,
+			'stricton' => false,
+			'failover' => array(),
+			'save_queries' => true
+		);
+
+/*defined('BASEPATH') OR exit('No direct script access allowed');
+$active_group = 'default';
+$query_builder = true;
+$dbhost = 'localhost';
+$db['default'] = array(
+	'dsn'	=> 'mysql:host=' . $dbhost . ';dbname=cstravel_main',
+	'hostname' => $dbhost,
+	'username' => 'cstravel_db',
+	'password' => 'admin@123',
+	'database' => 'cstravel_main', 
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => true,
+	'db_debug' => true,
+	'cache_on' => false,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => false,
+	'compress' => false,
+	'stricton' => false,
+	'failover' => array(),
+	'save_queries' => true
+);*/
